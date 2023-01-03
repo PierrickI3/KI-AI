58cf423623920e998d0c641d3192dc04c190d1b8
Pierrick Lozach <pierrick.lozach@genesys.com>
2023-01-03 09:25:08 +0000

    Update comments

--- GPT_J.py
+++ GPT_J.py
@@ -6,14 +6,23 @@ from transformers import GPTJForCausalLM, AutoTokenizer
 import torch
 
 #
-# CPU only, set to torch.float32 for CPU or torch.float16 for GPU
+# CPU only, set torch_dtype to torch.float32
+# To use GPU, set torch_dtype to torch.float16
 #
 model = GPTJForCausalLM.from_pretrained(
-    "EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float32, low_cpu_mem_usage=True)
+    "EleutherAI/gpt-j-6B", revision="float32", torch_dtype=torch.float32, low_cpu_mem_usage=True)
+
+# A tokenizer converts your input into a format that can be processed by the model. 
+# It converts text into a sequence of tokens, create a numerical represenation of the tokens and assemble them into tensors.
+# Tensors become model inputs.
+#
+# Here, we use the 6B parameters pre-trained model
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
-context = """In a shocking finding, scientists discovered a herd of unicorns living in a remote,
-            previously unexplored valley, in the Andes Mountains. Even more surprising to the
-            researchers was the fact that the unicorns spoke perfect English."""
+
+# context = """In a shocking finding, scientists discovered a herd of unicorns living in a remote,
+#            previously unexplored valley, in the Andes Mountains. Even more surprising to the
+#            researchers was the fact that the unicorns spoke perfect English."""
+context = """Genesys is a company that created a cloud-based contact center platform. What is Genesys?"""
 
 input_ids = tokenizer(context, return_tensors="pt").input_ids
 gen_tokens = model.generate(
